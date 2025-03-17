19d3032638327f8608ff3e5bd646a4dfb8ddd1f0
Gareth Brown <gareth.brown@appsoftware.com>
2025-03-17 11:26:15 +0000

    Job application progress

--- src/tests/jobserve-apply-net-csharp.spec.ts
+++ src/tests/jobserve-apply-net-csharp.spec.ts
@@ -14,27 +14,38 @@ test('jobserve-apply-net-csharp', async ({ page }) => {
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.getByRole('link', { name: 'My Account' }).click();
   await page.getByRole('link', { name: 'Job Searches & Alerts' }).click();
-  await page.getByRole('cell', { name: '.net c# asp.net Updated 6 Mar' }).click();
+  await page.getByRole('cell', { name: '.net c# asp.net' }).click(); // Must save with this name
   const page1Promise = page.waitForEvent('popup');
   await page.getByRole('link', { name: 'Run this search' }).click();
   const page1 = await page1Promise;
-  await page1.locator('#jsJobResContent').getByRole('heading', { name: 'C# .NET RX Developer - SOA' }).click();
-  await page1.locator('#td_apply_btn').click();
-  await page1.locator('#appFrame').contentFrame().locator('#filCV').click();
-  await page1.locator('#appFrame').contentFrame().locator('#filCV').setInputFiles('AppSoftware_Limited_Software_Engineer_CV_Gareth_Brown_Contract.docx');
-  await page1.locator('#appFrame').contentFrame().getByText('Dear Sir/Madam, Re: JSADL3274').click();
-  await page1.locator('#appFrame').contentFrame().getByText('Dear Sir/Madam, Re: JSADL3274').press('ControlOrMeta+a');
-  await page1.locator('#appFrame').contentFrame().getByText('Dear Sir/Madam, Re: JSADL3274').fill('Please see my CV attached. I would like to put myself forward for the advertised role. I\'m available for a new contract and the spec on the job posting looks like a a good match for my skill set and recent experience.\n\nContact me at gareth.brown@appsoftware.com or on +44(0)7730 590785.\n\nMany thanks,\n\nGareth Brown (Software Engineer)');
-  await page1.locator('#appFrame').contentFrame().getByRole('link', { name: 'Apply' }).first().click();
-  await page1.getByRole('button', { name: 'close' }).click();
-  await page1.getByRole('heading', { name: 'Risk C# Agile Developer -' }).click();
-  await page1.locator('#td_apply_btn').click();
-  await page1.locator('#appFrame').contentFrame().locator('#filCV').click();
-  await page1.locator('#appFrame').contentFrame().locator('#filCV').setInputFiles('AppSoftware_Limited_Software_Engineer_CV_Gareth_Brown_Contract.docx');
-  await page1.locator('#appFrame').contentFrame().getByText('Dear Sir/Madam, Re: JSADL3272').click();
-  await page1.locator('#appFrame').contentFrame().getByText('Dear Sir/Madam, Re: JSADL3272').press('ControlOrMeta+a');
-  await page1.locator('#appFrame').contentFrame().getByText('Dear Sir/Madam, Re: JSADL3272').fill('Please see my CV attached. I would like to put myself forward for the advertised role. I\'m available for a new contract and the spec on the job posting looks like a a good match for my skill set and recent experience.\n\nContact me at gareth.brown@appsoftware.com or on +44(0)7730 590785.\n\nMany thanks,\n\nGareth Brown (Software Engineer)');
-  await page1.locator('#appFrame').contentFrame().getByRole('link', { name: 'Apply' }).first().click();
-  await page1.getByRole('button', { name: 'close' }).click();
-  await page1.getByRole('button', { name: 'close' }).click();
+  
+  // Pause here
+
+  await page1.waitForTimeout(1000);
+
+  // Iterate over all h3 headings under #jsJobResContent
+  const headings = await page1.locator('#jsJobResContent h3').elementHandles();
+  for (const heading of headings) {
+    await heading.click();
+    await page1.waitForTimeout(2000); // wait for 2 seconds
+    const applyBtn = await page1.$('#td_apply_btn');
+    //const appliedBtn = await page1.$('#td_applied_btn');
+    if (!applyBtn) continue;
+    await page1.locator('#td_apply_btn').click();
+    
+    // Proceed with application process
+    const appFrame = await page1.locator('#appFrame').contentFrame();
+    //await appFrame.locator('#filCV').click();
+    await appFrame.locator('#filCV').setInputFiles('P:/App Software/Contractor CVs/AppSoftware_Limited_Software_Engineer_CV_Gareth_Brown_Contract.docx');
+    await appFrame.getByText('Dear Sir/Madam').click();
+    await appFrame.getByText('Dear Sir/Madam').press('ControlOrMeta+a');
+    await appFrame.getByText('Dear Sir/Madam').fill('Please see my CV attached. I would like to put myself forward for the advertised role. I\'m available for a new contract and the spec on the job posting looks like a a good match for my skill set and recent experience.\n\nContact me at gareth.brown@appsoftware.com or on +44(0)7730 590785.\n\nMany thanks,\n\nGareth Brown (Software Engineer)');
+    
+    // Just so I can see
+
+    await page1.waitForTimeout(1000);
+    
+    await appFrame.getByRole('link', { name: 'Apply' }).first().click();
+    await page1.getByRole('button', { name: 'close' }).click();
+  }
 });
